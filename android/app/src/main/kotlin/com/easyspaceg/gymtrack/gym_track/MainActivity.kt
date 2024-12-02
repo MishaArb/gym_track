24d096e53fdc2b5254431518a8e1d59e2334c6e1
user <opmisha@gmail.com>
2024-12-02 13:06:43 +0200

    Start project

---
+++ android/app/src/main/kotlin/com/easyspaceg/gymtrack/gym_track/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.easyspaceg.gymtrack.gym_track
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/presentation/ pages/training_plan/training_plan_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TrainingPlanScreen extends StatelessWidget {
+  const TrainingPlanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
